0bc873c1486fa552b8350c566a85ed72c528ca42
2101-farias-cyris <90444565+2101-farias-cyris@users.noreply.github.com>
2024-03-22 18:44:19 -0700

    Add files via upload

---
+++ Hw11.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hw11;
+
+public class Hw11 {
+    public static void main(String[] args) {
+        
+        String firstName = "Cyris";
+        String lastName = "Grouch";
+        String streetAddress = "206 medowood ct unit b";
+        String city = "Dayton";
+        String zipCode = "46543";
+        
+        System.out.println("First Name: " + firstName);
+        System.out.println("Last Name: " + lastName);
+        System.out.println("Street Address: " + streetAddress);
+        System.out.println("City: " + city);
+        System.out.println("Zip Code: " + zipCode);
+    }
+}
